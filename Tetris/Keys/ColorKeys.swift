88fdddde1b9ba239760758ea1169a1f6589e3486
diasjakupov <dsjakupov@gmail.com>
2024-05-09 20:23:18 +0500

    fixing

--- Tetris/Keys/ColorKeys.swift
+++ Tetris/Keys/ColorKeys.swift
@@ -4,7 +4,7 @@ import SwiftUI
 class ColorKeys{
     static var LTetromino: UInt32 = 0xa480cf
     static var LTetrominoInverted: UInt32 = 0xc5f6fb
-    static var skewTetromino: UInt32 = 0xff8877
+    static var skewTetromino: UInt32 = 0xd712ff
     static var skewTetrominoInverted: UInt32 = 0xffff82
     static var TTetromino: UInt32 = 0x6cccda
     static var straightTetromino: UInt32 = 0xb3e2a6
